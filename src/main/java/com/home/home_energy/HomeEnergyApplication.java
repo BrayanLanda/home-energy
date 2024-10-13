d9f475f202d09cbbd3d110313a5da193ba41c76d
Brayan Acosta <landakkpp@gmail.com>
2024-10-13 09:20:36 -0500

    first commit

---
+++ src/main/java/com/home/home_energy/HomeEnergyApplication.java
@@ -0,0 +1,13 @@
+package com.home.home_energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeEnergyApplication.class, args);
+	}
+
+}
