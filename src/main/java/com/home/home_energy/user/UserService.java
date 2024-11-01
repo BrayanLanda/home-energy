6d32c5677254f85a5eecc1ce6fa97ef77084f265
Brayan Acosta <landakkpp@gmail.com>
2024-10-31 23:14:30 -0500

    add update user and response-request user

--- src/main/java/com/home/home_energy/user/UserService.java
+++ src/main/java/com/home/home_energy/user/UserService.java
@@ -3,6 +3,8 @@ package com.home.home_energy.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
@@ -16,5 +18,18 @@ public class UserService {
         User newUser = new User(userRequest);
 
         return userRepository.save(newUser);
+    }
+
+    public Optional<User> authenticateUser(String email, String password){
+        Optional<User> user = userRepository.findByEmail(email);
+        return user.filter(u -> u.getPassword().equals(password));
+    }
+
+    public User updateUser(Long id, UpdateUserRequest updateUserRequest){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        user.setName(updateUserRequest.getName());
+        user.setPassword(updateUserRequest.getPassword());
+        return userRepository.save(user);
     }
 }
